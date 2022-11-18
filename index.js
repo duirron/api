4514b4d94e283b8539b8390b43d8179e5579cf4a
Luiz Alencastro <luiz.rodrigues@loglabdigital.com.br>
2022-11-18 17:36:59 -0400

    Update de projeto

--- index.js
+++ index.js
@@ -2,18 +2,45 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 //Query Params = ?nome=NodeJs
 //Route Params = /curso/2
 //Request Body = { nome: 'NodeJs', tipo: 'Backend'}
 
+const cursos = ['node JS', 'JavaScript'];
+
+server.get('/cursos', (req, res)=>{
+ return res.json(cursos)});
 //localhost:3000
-server.get('/cursos/:id', (req, res) => {
+server.get('/cursos/:index', (req, res) => {
 //Query Params const nome = req.query.nome;
 //Route Params const nome = req.query.id;
-//Request Body
+    const {index} = req.params;
+    return res.json(cursos[index]);
 
- const nome = req.query.id;
-    return res.json({curso: `id curso ${id}`});
+});
+
+server.post('/cursos', (req, res)=>{
+    const {name } = req.body;
+    cursos.push(name);
+
+    return res.json(cursos);
+})
+
+server.put('/cursos/:index', (req, res)=>{
+    const {index} = req.params;
+    const{name} = req.body;
+
+    cursos[index] = name;
+
+    return res.json(cursos);
+})
+
+server.delete('/cursos/:index', (req, res)=>{
+    const {index} = req.params;
+    cursos.splice(index, 1);
+    return res.json({ message: "curso deletado com sucesso"});
 })
 
 server.listen(3000);
